daaf4f21b2e7f2e29ba7917ada4866ab87a9eeeb
projectaki <akosegypro@gmail.com>
2020-09-18 01:02:10 +0200

    solve

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,48 @@ public class Main {
     public static void main(String[] args) {
 
         int[][] SUDOKU = new int[9][9];
-        SUDOKU[1][4] = 5;
-        SUDOKU[4][7] = 3;
-        SUDOKU[5][8] = 2;
+        SUDOKU[1][2] = 3;
+        SUDOKU[1][7] = 5;
+        SUDOKU[2][2] = 2;
+        SUDOKU[2][3] = 5;
+        SUDOKU[2][4] = 6;
+        SUDOKU[2][6] = 8;
+        SUDOKU[3][4] = 7;
+        SUDOKU[3][6] = 2;
+        SUDOKU[3][7] = 8;
+        SUDOKU[3][8] = 9;
+        SUDOKU[3][9] = 3;
+        SUDOKU[4][4] = 9;
+        SUDOKU[4][5] = 4;
+        SUDOKU[4][6] = 3;
+        SUDOKU[4][7] = 2;
+        SUDOKU[4][9] = 8;
+        SUDOKU[5][1] = 3;
+        SUDOKU[5][3] = 2;
+        SUDOKU[5][7] = 4;
+        SUDOKU[5][9] = 6;
+        SUDOKU[6][1] = 7;
+        SUDOKU[6][4] = 2;
+        SUDOKU[7][1] = 5;
+        SUDOKU[7][2] = 1;
+        SUDOKU[7][4] = 8;
+        SUDOKU[7][5] = 7;
+        SUDOKU[7][6] = 9;
+        SUDOKU[7][7] = 3;
+        SUDOKU[7][8] = 2;
+        SUDOKU[7][9] = 4;
+        SUDOKU[8][2] = 7;
+        SUDOKU[8][4] = 5;
+        SUDOKU[8][8] = 8;
+        SUDOKU[9][1] = 4;
+        SUDOKU[9][5] = 2;
+        SUDOKU[9][7] = 7;
+
+
         SudokuBoard sb = new SudokuBoard(SUDOKU);
 
         System.out.println(sb.isValid(4,9,2));
+        System.out.println(SUDOKU[5][5]);
+        sb.solve();
     }
 }
--- src/SudokuBoard.java
+++ src/SudokuBoard.java
@@ -1,17 +1,61 @@
 public class SudokuBoard {
     private final int[][] board;
+    private final boolean[][] immutable;
+    private int x;
+    private int y;
 
     public SudokuBoard(int[][] init)
     {
         board = new int[9][9];
+        immutable = new boolean[9][9];
         for (int i = 0; i < 9; i++)
         {
             for (int j = 0; j < 9; j++)
             {
                 board[i][j] = init[i][j];
+                if (init[i][j] != 0) immutable[i][j] = true;
             }
         }
+        x = 1;
+        y = 1;
 
+    }
+
+    public void solve()
+    {
+        solve(y);
+    }
+
+    private void solve(int j)
+    {
+        handlePosition();
+        if (x == 9 && y == 9) return;
+        if (immutable[x - 1][y - 1]) y++;
+        for (int numb = 1; numb <=9;numb++)
+        {
+
+            if (isValid(x,y,numb))
+            {
+                board[(x)-1][(y++)-1] = numb;
+                solve((y-1));
+            }
+        }
+        board[x - 1][(y--) - 1] = 0;
+        if (immutable[x - 1][y - 1]) y--;
+    }
+
+    private void handlePosition()
+    {
+        if (y == 10)
+        {
+            x++;
+            y = 1;
+        }
+        if (y == 0)
+        {
+            x--;
+            y = 9;
+        }
     }
 
     public boolean isValid(int i, int j, int numb)
