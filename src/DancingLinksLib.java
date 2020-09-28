34d43ca05f7c2adf77da9ae2367c1c7510f903fc
projectaki <akosegypro@gmail.com>
2020-09-28 13:39:52 +0200

    Link optimisation

--- src/DancingLinksLib.java
+++ src/DancingLinksLib.java
@@ -1,8 +1,6 @@
 import java.util.Stack;
 
 public class DancingLinksLib {
-    //  Matrix to hold the Nodes
-    private Node[][] NodeMatrix;
     // External library for constraints, options
     private ExactCoverLib lib;
     // Main header Node to start the search
@@ -52,31 +50,16 @@ public class DancingLinksLib {
         lib.fillOptionsBasedOnSudokuBoard();
         lib.fill4constraints();
         lib.fillCoverMatrix4Constraints();
-        NodeMatrix = new Node[lib.options.length + 1][lib.constraints.length];
         return lib.coverMatrix;
     }
 
 
     private void DLX(int[][] matrix,String[] constraintNames)
     {
-        fillMatrix(matrix, constraintNames);
+        coverMatrixToLinkedList(matrix, constraintNames);
         search();
     }
-/*
-// For testing on 2x2 sudoku board with 3 cosntraints on cell,row,col without box constraint
-    private void testDLX()
-    {
-        SudokuBoard boards = new SudokuBoard();
-        int[][] grid = boards.smallTestBoard();
-        exact = new ExactCoverMatrixSudoku(grid.length,3,grid);
-        exact.fillOptionsBasedOnSudokuBoard();
-        exact.fillConstraints();
-        NodeMatrix = new Node[exact.options.length + 1][exact.constraints.length];
-        fillCoverMatrix3Constraints(exact.constraints);
-        setupLinks(h);
-        search();
-    }
-*/
+
     // helper method for getting the solutions into the sudoku board
     // (takes a concat string and extracts the values for Row,Col and Val
     private int[] convertResultStringToBoardValues(String concatResult)
@@ -103,122 +86,16 @@ public class DancingLinksLib {
         }
         return numbs;
     }
-    // helper method for setting up links
-    // returns the "j" of the next available Node in the matrix row i
-    private int next(int i, int j)
-    {
-        int circleJ = (j+1) % lib.constraints.length;
-        while (NodeMatrix[i][circleJ] == null)
-        {
-            circleJ = (circleJ+1) % lib.constraints.length;
-        }
-        return circleJ;
-    }
 
-    // helper method for setting up links
-    // returns the "i" of the next available Node in the matrix col j
-    private int nextCol(int i, int j)
-    {
-        int circleI = (i+1) % (lib.options.length + 1);
-        while (NodeMatrix[circleI][j] == null)
-        {
-            circleI = (circleI+1) % (lib.options.length + 1);
-        }
-        return circleI;
-    }
-
-    // given a start Node and the Nodes i,j in Matrix => set up the circular double links in that row
-    private void setLinkRight(Node start, int i, int j)
-    {
-        Node x = NodeMatrix[i][j];
-        int cJ = j;
-        while (start.L == null)
-        {
-            int nextJ = next(i,cJ);
-            Node next = NodeMatrix[i][nextJ];
-            x.R = next;
-            next.L = x;
-            x = next;
-            cJ = nextJ;
-        }
-    }
-    // Given a start Node and the j value in Matrix => set up circular double links in column
-    // and if there is no links in the current Nodes row => also set up links for that row
-    private void setLinks(Node start, int j)
-    {
-        Node x = NodeMatrix[0][j];
-        int cI = 0;
-        while (start.U == null)
-        {
-            int nextI = nextCol(cI,j);
-            Node next = NodeMatrix[nextI][j];
-            x.D = next;
-            next.U = x;
-            if(next != NodeMatrix[0][j])
-            {
-                next.C = (ColumnNode) NodeMatrix[0][j];
-                next.C.S++;
-            }
-            if (x.R == null) setLinkRight(x,cI,j);
-            x = next;
-            cI = nextI;
-
-        }
-    }
-
-    // Starting at the Initial Node (Matrix[0][0]), go through all column headers and set up links for the whole matrix
-    // After links are set up also add the Node h
-    private void setupLinks()
-    {
-        for (int p = 0; p < lib.constraints.length; p++ ) {
-            // System.out.println(NodeMatrix[0][p].N);
-            setLinks(NodeMatrix[0][p], p);
-        }
-        NodeMatrix[0][0].L = h;
-        h.R = NodeMatrix[0][0];
-        NodeMatrix[0][lib.constraints.length - 1].R = h;
-        h.L = NodeMatrix[0][lib.constraints.length - 1];
-    }
-/*
-    // test method for filling Matrix with only 3 constraints for 2x2 board
-    public void fillCoverMatrix3Constraints(String[] constraints)
-    {
-        for(int z = 0;z < constraints.length;z++)
-        {
-            NodeMatrix[0][z] = new Node();
-            NodeMatrix[0][z].N = constraints[z];
-        }
-
-        for (int i = 1; i < exact.options.length + 1; i++) {
-            for (int j = 0; j < exact.constraints.length; j++) {
-                String firstHalf = Character.toString(exact.constraints[j].charAt(0)) + exact.constraints[j].charAt(1);
-                String secondHalf = exact.constraints[j].charAt(2) + Character.toString(exact.constraints[j].charAt(3));
-                if (exact.options[i - 1].contains(firstHalf) && exact.options[i - 1].contains(secondHalf))
-                {
-                    NodeMatrix[i][j] = new Node();
-                    NodeMatrix[i][j].N = Character.toString(exact.constraints[j].charAt(0)) + i;
-                }
-
-                else NodeMatrix[i][j] = null;
-            }
-        }
-    }
-*/
-    // Compare the options against contraints and fill out the intersections of options and constraints
-    // if its the first row (z=0) => set the Names of the Nodes to the constraint names as these are the headers
-    // for the 3/4 of the constraints we can directly compare the option against cosntraints and fill intersection
-    // for the last 1/4 (box constraints) we have to use a helper method (isOptionInBox) to see if the intersection exists
-    // that is because we cant directly compare eg: "R1C1" to "B1", the helper method disects B1 to R1C1-R3C3 etc..
-    private void fillMatrix(int[][] matrix, String[] constraints) {
+    private void coverMatrixToLinkedList(int[][] matrix, String[] constraints) {
         ColumnNode[] columns = new ColumnNode[constraints.length];
-        ColumnNode last = h;
+
         for (int i = 0; i < constraints.length; i++) {
             ColumnNode x = new ColumnNode(constraints[i]);
-            x.L = last;
-            x.R = last.R;
-            last.R.L = x;
-            last.R = x;
-            last = x;
+            x.L = h.L;
+            x.R = h;
+            h.L.R = x;
+            h.L = x;
             columns[i] = x;
         }
 
@@ -237,11 +114,10 @@ public class DancingLinksLib {
                     col.S++;
 
                     if (lastNode == null) lastNode = x;
-                    x.L = lastNode;
-                    x.R = lastNode.R;
-                    lastNode.R.L = x;
-                    lastNode.R = x;
-                    lastNode = x;
+                    x.L = lastNode.L;
+                    x.R = lastNode;
+                    lastNode.L.R = x;
+                    lastNode.L = x;
                 }
             }
         }
